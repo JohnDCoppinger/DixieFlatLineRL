edad2656b426ac82194c6f03f7d87b59a09fb93e
ewu-collinsb <brandon.collins@eagles.ewu.edu>
2015-02-13 21:07:48 -0800

    Test Dungeon Generator

---
+++ Tester.java
@@ -0,0 +1,14 @@
+
+public class Tester 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		dungeon d = new dungeon();
+		d.createDungeon(80, 20, 50);
+		System.out.print(d.showDungeon());
+		System.out.println("Map is done!");
+	}
+
+}
