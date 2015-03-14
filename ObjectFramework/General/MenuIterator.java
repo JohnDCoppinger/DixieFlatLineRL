4f2497c2a68883a188248f692cf6a1e9a203e5da
gallanoth <ohmgallanoth@gmail.com>
2015-03-14 15:47:58 -0700

    Created PlayerState for PlayerActionManager Input Created Menu Interface Created MenuItem Created InventoryMenu

---
+++ ObjectFramework/General/MenuIterator.java
@@ -0,0 +1,7 @@
+package ObjectFramework.General;
+
+/**
+ * Created by Gallanoth on 3/14/2015.
+ */
+public class MenuIterator {
+}
--- ObjectFramework/RL_Actors/PlayerActionManager.java
+++ ObjectFramework/RL_Actors/PlayerActionManager.java
@@ -1,11 +1,17 @@
 package ObjectFramework.RL_Actors;
 
+import ObjectFramework.General.MapView;
+
+import java.util.Observable;
+
 /**
  * Created by Gallanoth on 3/9/2015.
  */
-public class PlayerActionManager implements ActionManager{
+public class PlayerActionManager extends Observable implements ActionManager{
 
     private Actor client;
+    private MapView view;
+    private PlayerState playerState;
 
     private PlayerActionManager() {}
 
@@ -20,8 +26,12 @@ public class PlayerActionManager implements ActionManager{
     public int getInitiative() {
         return client.getInitiative();
     }
-    
+
     public int compareTo(ActionManager manager) {
         return client.getInitiative() - manager.getInitiative();
+    }
+
+    public void register(MapView view) {
+        this.view = view;
     }
 }
